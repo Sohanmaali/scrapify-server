8a03d17af3a6a2006cbd040622f227149bb13a37
sohan maali <sohanmaali4@gmail.com>
2025-04-27 12:14:28 +0530

    some changes

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@nestjs/common';
 export class AppService {
   getHello(): string {
 
-    return "<h1 align='center'>Hello i am Scrapify service <br> Created by Sohan 😎</h1";
+    return "<h1 align='center'>Hello i am Scrapify service <br> Created by Sohan 😎 </h1";
   }
 }
